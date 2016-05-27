692cb7b150868963c00bd07caaf3423224328904
zlsh80826 <zlsh80826@gmail.com>
2016-05-27 17:30:03 +0800

    modify error detect with errno

--- client.cpp
+++ client.cpp
@@ -8,6 +8,7 @@
 #include <sys/types.h>
 #include <time.h>
 #include <unistd.h>
+#include <errno.h>
 #define MAXLINE 4096
 using namespace std;
 
@@ -28,34 +29,37 @@ void str_cli(FILE* fp, int sockfd){
 }
 
 int main(int argc, char** argv){
+	// declare variable
 	int sockfd, n;
 	char recvline[MAXLINE + 1];
 	int child_number = 0;
 	struct sockaddr_in server_address;
 
+	//usage detect
 	if(argc!=3){
 		printf("usage:server_client <IP address>\n");
 		return 0;
 	}
 
-	if( (sockfd=socket(AF_INET, SOCK_STREAM, 0)) < 0)
-		printf("socket create error\n");
+	// create connect descriptor
+	if( (sockfd=socket(AF_INET, SOCK_STREAM, 0)) < 0 )
+		perror("socket create error");
 
-	bzero(&server_address, sizeof(server_address));
+	//setting server_address
+	bzero( &server_address, sizeof(server_address) );
 	server_address.sin_family=AF_INET;
 	server_address.sin_port=htons(atoi(argv[2]));
-	if(inet_pton(AF_INET, argv[1], &server_address.sin_addr) <= 0)
-		printf("inet_pton error for %s\n", argv[1]);
-
-	if(connect(sockfd, (sockaddr* )(&server_address), sizeof(server_address)) < 0)
-			printf("connect error\n");
-	str_cli(stdin, sockfd);
-	/*while( (n=read(sockfd, recvline, MAXLINE) ) ){
-		recvline[n]=0;
-		if(fputs(recvline, stdout) == EOF)
-			printf("fputs error");
+	if( inet_pton(AF_INET, argv[1], &server_address.sin_addr) <= 0 ){
+		printf("Can't identify IP address %s \n", argv[1]);
+		return 1;
 	}
-	if( n<0 )
-		printf("read error");*/
+
+	if( connect(sockfd, (sockaddr* )(&server_address), sizeof(server_address)) < 0 ){
+		perror("connect error");
+		return 1;
+	}
+
+	// main function
+	str_cli(stdin, sockfd);
 	return 0;
 }
