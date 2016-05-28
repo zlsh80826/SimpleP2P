995ca2f7e9b550918347e4e38977a825d4a10962
zlsh80826 <zlsh80826@gmail.com>
2016-05-28 21:50:11 +0800

    modify bytecount to byteCount

--- client.cpp
+++ client.cpp
@@ -21,20 +21,19 @@
 using namespace std;
 
 google::protobuf::uint32 readHdr(char *buf){
-  google::protobuf::uint32 size;
-  google::protobuf::io::ArrayInputStream ais(buf,4);
-  google::protobuf::io::CodedInputStream coded_input(&ais);
-  coded_input.ReadVarint32(&size);//Decode the HDR and get the size
-  cout<<"size of payload is "<<size<<endl;
-  return size;
+	google::protobuf::uint32 size;
+	google::protobuf::io::ArrayInputStream ais(buf,4);
+	google::protobuf::io::CodedInputStream coded_input(&ais);
+	coded_input.ReadVarint32(&size); //Decode the HDR and get the size
+	cout<<"size of payload is "<<size<<endl;
+	return size;
 }
 
-void readBody(int csock,google::protobuf::uint32 siz)
-{
-	int bytecount;
+void readBody(int csock,google::protobuf::uint32 siz){
+	int byteCount;
 	action::Login login;
 	char buffer [siz+4];
-  	if((bytecount = recv(csock, (void *)buffer, 4+siz, MSG_WAITALL))== -1){
+  	if( ( byteCount = recv(csock, (void *)buffer, 4+siz, MSG_WAITALL) )== -1 ){
         fprintf(stderr, "Error receiving data %d\n", errno);
     }
     google::protobuf::io::ArrayInputStream ais(buffer,siz+4);
