91805a86c2944e33e18493568ae6fab52a030b47
zlsh80826 <zlsh80826@gmail.com>
2016-05-30 22:07:30 +0800

    complete get_port

--- client_function.cpp
+++ client_function.cpp
@@ -13,6 +13,21 @@
 
 int get_port(int sockfd){
 	sendAction(sockfd, "portrequest");
+
+	int byteCount;
+	port::Port port;
+	char buffer[siz+4];
+  	if( ( byteCount = recv(csock, (void *)buffer, 4+siz, MSG_WAITALL) )== -1 ){
+        fprintf(stderr, "Error receiving data %d\n", errno);
+    }
+    google::protobuf::io::ArrayInputStream ais(buffer,siz+4);
+    google::protobuf::io::CodedInputStream coded_input(&ais);
+    coded_input.ReadVarint32(&siz);
+    google::protobuf::io::CodedInputStream::Limit msgLimit = coded_input.PushLimit(siz);
+    port.ParseFromCodedStream(&coded_input);
+    coded_input.PopLimit(msgLimit);
+    //std::cout << port.DebugString();
+    return port.port_num();
 }
 
 void connect_to_peer(int sockfd){
